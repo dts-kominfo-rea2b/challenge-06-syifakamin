fa09dc32f9c7041fa75b6e0f63a5dda05a527cd3
Amin Syifak <aminsyifak15@gmail.com>
2022-06-13 01:27:00 +0700

    completing challenge 06

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,8 @@
 // TODO: import module bila dibutuhkan di sini
-
+// menambahkan module file system (fs)
+const { error } = require('console');
+const fs = require('fs');
+const { resourceLimits } = require('worker_threads');
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +21,62 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+// Preprocessing Data
+const preprocessingData = (data) => {
+  let message = "";
+
+  if (data?.message != undefined) {
+    message = data?.message;
+  } else if (data?.length) {
+    data.forEach((item) => {
+      if (item?.message) {
+        message = item?.message;
+      }
+
+      if (item?.data?.message != undefined) {
+        message = item?.data?.message;
+
+      }
+    });
+  }
+
+  return message.split(" ")[1];
+}
+
+// Inisialisasi function bacaData
+const bacaData = (fnCallBack) => {
+  let hasil = [];
+  fs.readFile(file1, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      fnCallBack(err, null);
+      return;
+    }
+    hasil.push(preprocessingData(JSON.parse(data)));
+
+
+    fs.readFile(file2, { encoding: "utf8" }, (err, data) => {
+      if (err) {
+        fnCallBack(err, null);
+        return;
+      }
+      hasil.push(preprocessingData(JSON.parse(data)));
+
+
+      fs.readFile(file3, { encoding: "utf8" }, (err, data) => {
+        if (err) {
+          fnCallBack(err, null);
+          return;
+        }
+
+        hasil.push(preprocessingData(JSON.parse(data)));
+
+
+        fnCallBack(err, hasil);
+      })
+    })
+  })
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
